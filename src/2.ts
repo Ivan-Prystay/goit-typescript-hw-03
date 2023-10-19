755a19c1587a9b5a1b8a856b938c8b56a3a15211
Ivan Prystay <prystay.ivan@gmail.com>
2023-10-20 00:59:29 +0300

    fin task #2

--- src/2.ts
+++ src/2.ts
@@ -16,24 +16,31 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
 
-  constructor(name: string, department: string, salary: number) {
-    this.name = name;
-    this.department = department;
-    this.salary = salary;
-  }
+  constructor(
+    public name: string,
+    private department: string,
+    protected salary: number
+  ) {}
 
-  getEmployeeDetails() {
+  getEmployeeDetails(): string {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
   }
 }
 
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(personName: string, salary: number) {
+    super(personName, "X-Dept", salary + 10000);
+  }
 }
 
+const manager = new Manager("Ivan", 800);
+
+console.log(
+  "manager.getEmployeeDetails(): ",
+  typeof manager.getEmployeeDetails()
+);
+
+console.log("manager.getEmployeeDetails(): ", manager.getEmployeeDetails());
 
 export {};
